2ee269bde776c92797987b64658c001d7c4ec156
icolwell <icolwell@gsd.uwaterloo.ca>
2016-03-17 18:14:57 -0400

    Some small progress on the path planner

--- path_planner.cpp
+++ path_planner.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "path_planner");
 	ros::NodeHandle n;
-	
+
 	tf::TransformBroadcaster broadcaster;
 
 	ros::Rate loop_rate(10);
@@ -18,8 +18,8 @@ int main(int argc, char** argv)
 			tf::StampedTransform(
 				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.5, 0.0, 0.5)),
 				ros::Time::now(), "base_link", "scan"));
-		
-		
+
+
 
 
 		// Chill until the next loop
